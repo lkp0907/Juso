2acd1fa3fc6ebe3ad050e7dc055736e94677e629
GiPyeong Lee <lkp0907@gmail.com>
2015-03-07 17:24:28 +0900

    init project

---
+++ txtToSQL/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  txtToSQL
+//
+//  Created by GiPyeong Lee on 2015. 3. 7..
+//  Copyright (c) 2015년 com.devsfolder.Hongik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
